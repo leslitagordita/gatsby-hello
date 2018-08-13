621c3bf2bb049b9d583170d8a1ca377bb211a9d8
leslitagordita <contact@lesliesalazar.com>
2018-08-13 16:23:08 -0400

    Add ssh values to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-set -e
+#set -e
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 scripts/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
+
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
