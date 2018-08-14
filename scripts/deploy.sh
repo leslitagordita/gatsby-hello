7f3082708896729e5af4296431e14ee92256a420
leslitagordita <contact@lesliesalazar.com>
2018-08-13 20:22:19 -0400

    Add gatsby dependencies (#7)
    * Add gatsby dependencies
    
    * Remove sudo from npm command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,10 @@
 #chmod 600 scripts/id_rsa # Allow read access to the private key
 #ssh-add .scripts/id_rsa # Add the private key to SSH
 
+gatsby build
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
 git remote add production ssh://lsalazar@173.255.204.36:/usr/share/nginx/html/docteamdemosite2
 git push production master # push our updates
-gatsby build
+#gatsby build
