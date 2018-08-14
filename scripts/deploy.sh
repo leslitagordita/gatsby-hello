74cc73f0a5e521e7eb05d25b799f7b4ad0fd7e27
leslitagordita <contact@lesliesalazar.com>
2018-08-14 16:27:29 -0400

    Update deploy directory (#15)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,8 @@ gatsby build
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
-git remote add production ssh://lsalazar@173.255.204.36:/usr/share/nginx/html/docteamdemosite2
+#git remote add production ssh://lsalazar@173.255.204.36:/usr/share/nginx/html/docteamdemosite2
+git remote add production ssh://lsalazar@173.255.204.36:/home/lsalazar/gatsbybare.git
 git add . && git commit -m "Gatsby build"
 git push production master # push our updates
 #gatsby build
